0cc200d64cda5b55cee39cdd00d1a927f6dc7c7d
edarichev <edarichev@gmail.com>
2024-01-03 15:19:19 +0300

    CAR 4.6

--- car/car_4_6.h
+++ car/car_4_6.h
@@ -12,57 +12,65 @@ public:
 
     static BNode *getNextNode(BNode *n)
     {
+        // если ничего нет
         if (n == nullptr)
             return nullptr;
-        if (n->right != nullptr) {
-            return leftMostChild(n->right);
-        } else {
-            BNode *q = n;
-            BNode *x = q->parent;
-            while (x != nullptr && x->left != q) {
-                q = x;
-                x = x->parent;
-            }
-            return x;
-        }
-    }
-
-    static BNode *leftMostChild(BNode *n)
-    {
-        if (n == nullptr)
+        // это корень, тоже ничего нет
+        auto q = n->parent;
+        if (q == nullptr)
             return nullptr;
-        while (n->left != nullptr) {
-            n = n->left;
+        if (n == q->left) {
+            // это левый узел, попробуем найти правый
+            if (q->right)
+                return q->right;
         }
-        return n;
+        // иначе это правый узел, поднимаемся
+        int deep = 1;
+        do {
+            q = q->parent;
+            if (!q)
+                return nullptr;
+            auto found = getNode(q->right, deep);
+            if (found)
+                return found;
+            deep++;
+        } while (true);
+        return nullptr;
     }
 
-    static void printResult(BNode *n1)
-    {
-        if (n1 == nullptr)
-            cout << "null" << endl;
-        else
-            cout << n1->value << endl;
+    static BNode *getNode(BNode *q, int deep) {
+        if (deep == 0)
+            return q;
+        if (!q)
+            return nullptr;
+        BNode *r = nullptr;
+        if (q->left)
+            r = getNode(q->left, deep - 1);
+        if (r)
+            return r;
+        if (q->right)
+            r = getNode(q->right, deep - 1);
+        return r;
     }
 
     static void test()
     {
-        BNode *topValid = new BNode(20);
-        topValid->addLeft(10);
-        auto f5 = topValid->left->addLeft(5);
-        auto f3 = topValid->left->left->addLeft(3);
-        auto f7 = topValid->left->left->addRight(7);
-        auto f15 = topValid->left->addRight(15);
-        auto f17 = topValid->left->right->addRight(17);
+        BNode *topValid = new BNode(8);
+        auto f4 = topValid->addLeft(4);
+        auto f2 = f4->addLeft(2);
+        auto f6 = f4->addRight(6);
+        auto f10 = topValid->addRight(10);
+        auto f11 = f10->addLeft(11);
+        auto f20 = f10->addRight(20);
 
         printBTree(topValid);
 
-        auto n1 = getNextNode(f5);
-        ASSERT(n1->value == 15);
-        n1 = getNextNode(f3);
-        ASSERT(n1->value == 7);
-        n1 = getNextNode(f7);
-        ASSERT(n1->value == 17);
+        auto n1 = getNextNode(f2);
+        ASSERT(n1->value == 6);
+        n1 = getNextNode(f6);
+        ASSERT(n1->value == 11);
+        n1 = getNextNode(f4);
+        ASSERT(n1->value == 10);
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
 //#include "leetcode/c0136_Single_Number.h"
 //#include "saj/p1_1_33_Matrix.h"
-//#include "car/car_4_6.h"
-#include "alg/sort_shell.h"
+#include "car/car_4_6.h"
+// #include "alg/sort_shell.h"
 
 using namespace std;
-
-struct C1
-{
-    using fnname = void(C1::*)(int);
-    void fn1(int){}
-};
 
 int main()
 {
     //test(); // leetcode
     //Solution::test();
     //test_sort_shell();
-    cout << sizeof(int*);
+    CAR_4_6().test();
     return 0;
 }
